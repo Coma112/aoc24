47b0b6e47234dd375e312c45f7e824ff6a0b00e3
kulas.dominik <kulas.dominik@ckik.loc>
2024-12-02 08:42:28 +0100

    genyozas

--- 2/1.py
+++ 2/1.py
@@ -3,24 +3,16 @@ with open('aoc24/2/input.txt') as f:
 
 safeCount = 0
 
-def isSafe(numberRange: str):
-    numberList = list(map(int, numberRange.split(" ")))
-    
-    isIncreasing = True
-    isDecreasing = True
+def isSafe(levels):
+    numberList = list(map(int, levels.split(" ")))
     
     for i in range(len(numberList) - 1):
         difference = abs(numberList[i] - numberList[i + 1])
         
-        if difference < 1 or difference > 3:
+        if ((difference < 1) or (difference > 3)):
             return False
-        
-        if numberList[i] < numberList[i + 1]:
-            isDecreasing = False
-        elif numberList[i] > numberList[i + 1]:
-            isIncreasing = False
     
-    return isIncreasing or isDecreasing
+    return all(earlier < later for earlier, later in zip(numberList, numberList[1:])) or all(earlier >= later for earlier, later in zip(numberList, numberList[1:]))
 
 for line in lines:
     if isSafe(line):
--- 2/2.py
+++ 2/2.py
@@ -1,32 +1,21 @@
-with open('aoc24/2/input.txt') as f:
-    lines = f.readlines()
-
+lines = open("aoc24/2/input.txt").read().splitlines()
 safeCount = 0
 
 def isSafe(levels):
     numberList = list(map(int, levels.split(" ")))
-    
-    isIncreasing = True
-    isDecreasing = True
     
     for i in range(len(numberList) - 1):
         difference = abs(numberList[i] - numberList[i + 1])
         
         if ((difference < 1) or (difference > 3)):
             return False
-        
-        if (numberList[i] < numberList[i + 1]):
-            isDecreasing = False
-        elif (numberList[i] > numberList[i + 1]):
-            isIncreasing = False
     
-    return isIncreasing or isDecreasing
+    return all(earlier < later for earlier, later in zip(numberList, numberList[1:])) or all(earlier >= later for earlier, later in zip(numberList, numberList[1:]))
 
 def canBeMadeSafe(levels):
     levelList = list(map(int, levels.split(" ")))
-    n = len(levelList)
     
-    for i in range(n):
+    for i in range(len(levelList)):
         newLevels = levelList[:i] + levelList[i + 1:]
 
         if (isSafe(" ".join(map(str, newLevels)))):
